3131ca9df35c69bdcc9c74441afe9923a7d40e27
Damian <damian.niemiec@gmail.com>
2020-06-16 18:44:06 +0200

    Fixed phpmyadmin setup for Ubuntu, added fixes for Ubuntu 20

--- bin/ansible_setup.sh
+++ bin/ansible_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# WSL config
 if [ -d /mnt/c/ ]; then
     if [ ! -f /etc/wsl.conf ]; then    
         echo "[automount]
@@ -10,12 +12,38 @@ if [ -d /mnt/c/ ]; then
     fi
 fi
 
-apt update
-apt install software-properties-common pwgen nano python python-pip -y
-apt-add-repository --yes --update ppa:ansible/ansible
-apt update
-apt install ansible -y
+# Check distribution and version
+Distribution=$(lsb_release -i --short)
+Version=$(lsb_release -r --short)
 
-if [ ! -f secrets_pass_file.txt ]; then
-	pwgen 32 1 -y > secrets_pass_file.txt
+# For Debian and Ubuntu
+if [ "$Distribution" == "Ubuntu" ] || [ "$Distribution" = "Debian" ]; then
+    apt -q update
+    apt -q upgrade -y
+    apt -qq install software-properties-common pwgen nano -y
+
+    # Ubuntu specific operations
+    if [ "$Distribution" == 'Ubuntu' ]; then
+        if [ "$(printf '%s\n' "20" "$Version" | sort -V | head -n1)" = "20" ]; then
+            apt -qq install python2 -y
+            curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
+            python2 /tmp/get-pip.py
+        else
+            apt -qq install python python-pip -y
+            apt-add-repository --yes --update ppa:ansible/ansible
+        fi
+    fi
+    # Debian specific operations
+    if [ "$Distribution" == 'Debian' ]; then
+        grep -qxF 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' /etc/apt/sources.list || echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | tee -a /etc/apt/sources.list > /dev/null
+        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+        apt -q update
+    fi
+    apt -qq install ansible -y
+
+    if [ ! -f secrets_pass_file.txt ]; then
+        pwgen 32 1 -y > secrets_pass_file.txt
+    fi
+else
+    echo "This script works only with Debian and Ubuntu (for now)"
 fi
